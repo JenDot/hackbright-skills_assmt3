6dff7b310cc820d6a4c165ce6e44f563e3d1c8e1
Keiva <jen@silverwire.ninja>
2021-01-24 17:30:12 -0700

    Stopped tracking .vscode; added Errors.txt; Completed Customizing the User Experience section.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import render_template
 from flask import Flask, redirect, request, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
+from flask import Flask, session
 
 
 app = Flask(__name__)
@@ -16,30 +17,52 @@ app.jinja_env.undefined = StrictUndefined
 app.jinja_env.auto_reload = True
 
 # Required to use Flask sessions and the debug toolbar
-app.secret_key = 'ABC'
+app.secret_key = "ABC"
 
 MOST_LOVED_MELONS = {
-    'cren': {
-        'img': 'http://www.rareseeds.com/assets/1/14/DimRegular/crenshaw.jpg',
-        'name': 'Crenshaw',
-        'num_loves': 584,
+    "cren": {
+        "img": "http://www.rareseeds.com/assets/1/14/DimRegular/crenshaw.jpg",
+        "name": "Crenshaw",
+        "num_loves": 584,
     },
-    'jubi': {
-        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Jubilee-Watermelon-web.jpg',
-        'name': 'Jubilee Watermelon',
-        'num_loves': 601,
+    "jubi": {
+        "img": "http://www.rareseeds.com/assets/1/14/DimThumbnail/Jubilee-Watermelon-web.jpg",
+        "name": "Jubilee Watermelon",
+        "num_loves": 601,
     },
-    'sugb': {
-        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Sugar-Baby-Watermelon-web.jpg',
-        'name': 'Sugar Baby Watermelon',
-        'num_loves': 587,
+    "sugb": {
+        "img": "http://www.rareseeds.com/assets/1/14/DimThumbnail/Sugar-Baby-Watermelon-web.jpg",
+        "name": "Sugar Baby Watermelon",
+        "num_loves": 587,
     },
-    'texb': {
-        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Texas-Golden-2-Watermelon-web.jpg',
-        'name': 'Texas Golden Watermelon',
-        'num_loves': 598,
+    "texb": {
+        "img": "http://www.rareseeds.com/assets/1/14/DimThumbnail/Texas-Golden-2-Watermelon-web.jpg",
+        "name": "Texas Golden Watermelon",
+        "num_loves": 598,
     },
 }
+
+"""In server.py, write a route that serves the template homepage.html at the route /. """
+
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+"""At the route /get-name:
+    * Get the name that the user submitted (from request.args).
+    * Add the user’s name to the session (remember, a session operates very much like a dictionary, so you can add a name
+        to the session the same way you would add, for example, the key ‘name’ with the value ‘Balloonicorn’ to a regular Python
+        dictionary).
+    * After the name has been added to the session, redirect to the /top-melons route.
+"""
+
+
+@app.route("/get-name")
+def get_name():
+    person = session["person"]
+    return redirect("/top-melons")
 
 
 """Write a route, /top-melons, that renders the template top-melons.html.
@@ -48,17 +71,17 @@ MOST_LOVED_MELONS = {
 """
 
 
-@app.route('/top-melons')
+@app.route("/top-melons")
 def top_melons():
 
     return render_template("top-melons.html", melons=MOST_LOVED_MELONS)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
     app.debug = True
 
     DebugToolbarExtension(app)
 
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
